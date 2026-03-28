27e70078bdde8dd25c0f266067265f805ef92e54
Antonia Villamil Rodriguez <anto.villamil2008@gmial.com>
2026-03-28 13:05:05 -0500

    8

--- industrial_temperature.py
+++ industrial_temperature.py
@@ -1,1 +1,7 @@
-
+def trigger_alarm(temperatures, threshold=80):
+    sensores_en_alerta = []
+    for nombre_sensor, lectura_actual in temperatures.items():
+        # Verificamos si la lectura supera estrictamente el límite
+        if lectura_actual > threshold:
+            sensores_en_alerta.append(nombre_sensor)
+    return sensores_en_alerta
